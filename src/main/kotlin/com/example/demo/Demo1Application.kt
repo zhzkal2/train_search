e54dc8b92585e1c2fdd0c468a98dfe958d5fc67e
zhzkal2 <reliya1541@gmail.com>
2024-07-20 05:35:22 +0900

    slack api 추가 테스트 코드 작성

--- src/main/kotlin/com/example/demo/Demo1Application.kt
+++ src/main/kotlin/com/example/demo/Demo1Application.kt
@@ -1,5 +1,6 @@
 package com.example.demo
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
@@ -8,7 +9,13 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @SpringBootApplication
 @EnableScheduling
 @EnableConfigurationProperties(TrainConfig::class)
-class Demo1Application
+class Demo1Application(val slackService: SlackServiceImpl) : CommandLineRunner {
+
+    override fun run(vararg args: String?) {
+        slackService.showSlackInfo()
+        slackService.sendMessage("테스트 메시지")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Demo1Application>(*args)
